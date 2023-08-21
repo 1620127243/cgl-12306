698149c7e314d2546d74fe6d3272c6b85386a107
cgl <1620127243@qq.com>
2023-08-21 16:30:20 +0800

    初始化

---
+++ src/test/java/com/cgl/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgl.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
