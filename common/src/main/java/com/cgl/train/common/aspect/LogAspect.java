99a5b08b28f0924eb47278c53cd4765e3bb878eb
cgl <1620127243@qq.com>
2023-08-21 20:50:26 +0800

    增加通用模块，存放公用的工具类、拦截器等

--- common/src/main/java/com/cgl/train/common/aspect/LogAspect.java
+++ common/src/main/java/com/cgl/train/common/aspect/LogAspect.java
@@ -1,4 +1,4 @@
-package com.cgl.train.member.aspect;
+package com.cgl.train.common.aspect;
 
 import cn.hutool.core.util.RandomUtil;
 import com.alibaba.fastjson.JSONObject;
--- member/src/main/java/com/cgl/train/member/MemberApplication.java
+++ member/src/main/java/com/cgl/train/member/MemberApplication.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 
 @SpringBootApplication
+@ComponentScan("com.cgl")
 public class MemberApplication {
     private static final Logger LOG= LoggerFactory.getLogger(MemberApplication.class);
 
