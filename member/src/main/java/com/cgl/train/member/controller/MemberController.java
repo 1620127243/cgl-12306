1e6c2f3f0abe3808bc0df68202d95906dc244220
cgl <1620127243@qq.com>
2023-08-22 16:07:41 +0800

    封装返回值

--- member/src/main/java/com/cgl/train/member/controller/MemberController.java
+++ member/src/main/java/com/cgl/train/member/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.cgl.train.member.controller;
 
+import com.cgl.train.common.response.CommonResp;
+import com.cgl.train.member.req.MemberRegisterReq;
 import com.cgl.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +15,15 @@ public class MemberController {
     @Resource
     private MemberService memberService;
     @GetMapping("/count")
-    public int getCount(){
-        return memberService.count();
+    public CommonResp<Integer> getCount(){
+        CommonResp<Integer> commonResp=new CommonResp<>();
+        commonResp.setContent(memberService.count());
+        return commonResp;
+    }
+    @PostMapping("/register")
+    public CommonResp<Long> register(MemberRegisterReq req){
+        CommonResp<Long> commonResp=new CommonResp<>();
+        commonResp.setContent(memberService.register(req));
+        return commonResp;
     }
 }
--- member/src/main/java/com/cgl/train/member/service/MemberService.java
+++ member/src/main/java/com/cgl/train/member/service/MemberService.java
@@ -1,15 +1,37 @@
 package com.cgl.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.cgl.train.member.domain.Member;
+import com.cgl.train.member.domain.MemberExample;
 import com.cgl.train.member.mapper.MemberMapper;
+import com.cgl.train.member.req.MemberRegisterReq;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
     @Resource
     private MemberMapper memberMapper;
-    public int count(){
+    public Integer count(){
         return (int) memberMapper.countByExample(null);
+    }
+    public long register(MemberRegisterReq req){
+        String mobile=req.getMobile();
+        MemberExample example=new MemberExample();
+        example.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list= memberMapper.selectByExample(example);
+        if(CollUtil.isNotEmpty(list)) {
+//            return list.get(0).getId();
+            throw new RuntimeException("手机号已注册");
+
+        }
+        Member member=new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 
 }
